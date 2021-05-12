f39fb725e4ef04b12c71fb6af6e3bc347de4785d
chaeyeon10 <pcy9472@gmail.com>
2021-05-12 16:43:57 +0900

    update: chaeyeon

--- CL_first_project/chaeyeon/views.py
+++ CL_first_project/chaeyeon/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def home(request):
+#     posts = Post.objects.all()
+#     return render(request, 'home.html', {'posts' : posts})
+
+def home(request):
+    return render(request, 'home.html')
